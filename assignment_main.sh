7c37e7a68600ea652809acc2d9bf90cf376a4bb5
ArisaOkuyama <arisaokuyama@gmail.com>
2020-03-13 15:40:28 +1000

    changed test case

--- assignment_main.sh
+++
@@ -1,8 +0,0 @@
-echo “Welcome to Coder Coffee”
-echo “Lets begin the install now”
-git clone “https://github.com/ArisaOkuyama/ArisaOkuyama_T1A2.git”
-cd assignment_terminal/src
-echo “Time to ensure all required gems are installed”
-bundle install
-echo “All gems have been installed, running application for the first time now”
-ruby assignment_main.rb
--- test.rb
+++ test.rb
@@ -1,21 +1,21 @@
 require 'test/unit'
-require "tty-box"
 require_relative 'coffee'
 
 class TestSample < Test::Unit::TestCase
   def test_coffee_size_small
     coffee = Coffee.new("Flat White", 'Small',"Full Milk",'1 Sugar','No thank you')
-    assert_equal 'Small', coffee.size
+    assert_equal 'No thank you', coffee.extra
   end
   
-  # A small normal coffee is $3.50
-  def test_coffee_size_price
-    coffee = Coffee.new("Flat White", 'Small',"Full Milk",'1 Sugar','No thank you')
-    assert_equal 3.5, coffee.get_price
+  # A small Almond milk coffee is $3.50
+  def test_coffee_price
+    coffee = Coffee.new("Flat White", 'Small',"Almond Milk",'1 Sugar','No thank you')
+    assert_equal 4.0, coffee.get_price
+  end
+  # A large normal milk coffee is $5
+  def test_price_large
+    coffee = Coffee.new("Flat White", 'Large',"Full Milk",'4 Sugar','No thank you')
+    assert_equal 5, coffee.get_price
   end
   
-  def test_coffee_size_price
-    coffee = Coffee.new("Flat White", 'Small',"Full Milk",'1 Sugar','No thank you')
-    assert_equal 3.5, coffee.get_price
-  end
 end
